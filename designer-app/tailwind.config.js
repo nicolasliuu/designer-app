7e707143344c727c1fc9609bc84d3aa211839e02
Fabio Fernandez <ffernan9@jhu.edu>
2024-09-27 14:12:12 -0400

    simplify tailwind es6 exports

--- designer-app/tailwind.config.js
+++ designer-app/tailwind.config.js
@@ -1,21 +1,17 @@
-/** @typedef {import("tailwindcss").Config} TailwindConfig */
-
-/** @type {TailwindConfig["content"]} */
-export const content = [
-  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-];
-
-/** @type {TailwindConfig["theme"]} */
-export const theme = {
-  extend: {
-    colors: {
-      background: "var(--background)",
-      foreground: "var(--foreground)",
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
     },
   },
+  plugins: [],
 };
-
-/** @type {TailwindConfig["plugins"]} */
-export const plugins = [];
