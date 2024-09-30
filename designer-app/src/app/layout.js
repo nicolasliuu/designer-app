4e13c414c1f11c5961458184b18a59f4bfe7fcd9
Fabio Fernandez <ffernan9@jhu.edu>
2024-09-30 17:26:55 -0400

    add input and submit btn for prompt > styles set in global.css for now > use rubik font (can be changed later)

--- designer-app/src/app/layout.js
+++ designer-app/src/app/layout.js
@@ -1,15 +1,10 @@
-import localFont from "next/font/local";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const rubik = Rubik({
+  subsets: ["latin-ext"],
+  variable: "--font-rubik",
+  weight: "variable",
 });
 
 export const metadata = {
@@ -20,11 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${rubik.variable} antialiased`}>{children}</body>
     </html>
   );
 }
