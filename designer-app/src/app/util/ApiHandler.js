79ba0df265436082e0192b2ad55f65b13ab88b95
Fabio Fernandez <ffernan9@jhu.edu>
2024-10-09 15:14:37 -0400

    create API handler builder > make route handlers more readable > API verb methods independently defined > to-do: define global middleware

---
+++ designer-app/src/app/util/ApiHandler.js
@@ -0,0 +1,77 @@
+/**
+ * @typedef {import("next").PageConfig | undefined} ApiConfig
+ *
+ * @typedef {(typeof METHODS)[number]} ApiMethod
+ *
+ * @typedef {(
+ *   req: import("next").NextApiRequest,
+ *   res: import("next").NextApiResponse,
+ * ) => Promise<void>} ApiHandler
+ *
+ *
+ * @typedef {(handler: ApiHandler) => ApiHandlerBuilder} ApiHandlerSetter
+ *
+ * @typedef {{
+ *   [M in ApiMethod]: ApiHandlerSetter;
+ * } & {
+ *   handlers: { [M in ApiMethod]: ApiHandler };
+ *   set: (method: ApiMethod, handler: ApiHandler) => ApiHandlerBuilder;
+ *   build: () => ApiHandler;
+ *   buildWithConfig: (config: ApiConfig) => {
+ *     handler: ApiHandler;
+ *     config: ApiConfig;
+ *   };
+ * }} ApiHandlerBuilder
+ */
+
+const METHODS = /** @type {const} */ ([
+  "GET",
+  "POST",
+  "PUT",
+  "PATCH",
+  "DELETE",
+  "HEAD",
+  "OPTIONS",
+]);
+
+/** @type {function(): ApiHandlerBuilder} */
+function ApiHandler() {
+  /** @ts-ignore @type {ApiHandlerBuilder} */
+  const routeBuilder = {
+    /** @ts-ignore @type {{ [M in ApiMethod]: ApiHandler }} */
+    handlers: {},
+
+    set(method, handler) {
+      this.handlers[method] = handler;
+      return this;
+    },
+
+    /** @returns {ApiHandler} */
+    build() {
+      return async (req, res) => {
+        const method = this.handlers[req.method];
+
+        if (!method) {
+          res.status(405).json({ error: `Method ${req.method} not allowed` });
+          return;
+        }
+        await method?.(req, res);
+      };
+    },
+
+    buildWithConfig(config = undefined) {
+      return {
+        config,
+        handler: this.build(),
+      };
+    },
+  };
+
+  METHODS.forEach((method) => {
+    routeBuilder[method] = (handler) => routeBuilder.set(method, handler);
+  });
+
+  return routeBuilder;
+}
+
+export { ApiHandler as default };
