06b838a5280f0bf37474f61c0308e318562ca34b
Nicolas Liu <nicolasliu18@gmail.com>
2024-10-21 15:43:37 -0400

    header goes to home page in collections

--- designer-app/src/components/Header.js
+++ designer-app/src/components/Header.js
@@ -11,16 +11,11 @@ import Link from "next/link";
 export default function Header(props) {
   const { title = "TITLE" } = props;
 
-  const handleClick = (e) => {
-    e.preventDefault(); // Prevent the default link behavior
-    window.location.reload(); // Trigger a full page reload
-  };
-
   return (
     <header className="header">
-      <a href="/" onClick={handleClick}>
-        <h1 className="title">{title}</h1>
-      </a>
+      <Link href="/">
+          <h1 className="title">{title}</h1>
+      </Link>
     </header>
   );
 }
