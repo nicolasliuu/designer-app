56242ad77ca300d7e95047d6e13910abca7d5f5b
Fabio Fernandez <ffernan9@jhu.edu>
2024-10-23 15:45:46 -0400

    refactor tailwind colors, add background > fabric texture bg tile > update color palette

--- designer-app/tailwind.config.js
+++ designer-app/tailwind.config.js
@@ -2,32 +2,39 @@
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: {
+          main: "var(--background-main)",
+          alt: "var(--background-alt)",
+        },
         foreground: "var(--foreground)",
-        accent: "var(--accent)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          darkest: "var(--primary-darkest)",
+          darker: "var(--primary-darker)",
+          dark: "var(--primary-dark)",
+          light: "var(--primary-light)",
+          lighter: "var(--primary-lighter)",
+          lightest: "var(--primary-lightest)",
+        },
+
         secondary: "var(--secondary)",
+        accent: "var(--accent)",
 
-        "color-darkest": "var(--color-darkest)",
-        "color-darker": "var(--color-darker)",
-        "color-dark": "var(--color-dark)",
-        "color-light": "var(--color-light)",
-        "color-lighter": "var(--color-lighter)",
-        "color-lightest": "var(--color-lightest)",
+        disabled: {
+          "color-darkest": "var(--disabled-primary-darkest)",
+          "color-darker": "var(--disabled-primary-darker)",
+          "color-dark": "var(--disabled-primary-dark)",
+          "color-light": "var(--disabled-primary-light)",
+          "color-lighter": "var(--disabled-primary-lighter)",
+          "color-lightest": "var(--disabled-primary-lightest)",
+        },
 
-        "disabled-color-darkest": "var(--disabled-color-darkest)",
-        "disabled-color-darker": "var(--disabled-color-darker)",
-        "disabled-color-dark": "var(--disabled-color-dark)",
-        "disabled-color-light": "var(--disabled-color-light)",
-        "disabled-color-lighter": "var(--disabled-color-lighter)",
-        "disabled-color-lightest": "var(--disabled-color-lightest)",
-
-        "color-active-fill": "var(--color-active-fill)",
+        "primary-active-fill": "var(--primary-active-fill)",
 
         "color-input-text": "var(--color-input-text)",
         "color-input-placeholder": "var(--color-input-placeholder)",
