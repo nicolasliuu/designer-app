a609b2c7b77cc8c04783844ac8a3768de5badc5d
Fabio Fernandez <ffernan9@jhu.edu>
2024-11-08 14:46:53 -0500

    updates to root layout > add new garment/collection buttons > refactor button custom props > add collection name overflow tooltip

--- designer-app/src/components/RootLayout.js
+++ designer-app/src/components/RootLayout.js
@@ -9,9 +9,13 @@ import React, { createContext, useState } from "react";
 /**
  * @typedef {{ title: string; back?: string }} HeaderState
  * @type {React.Context<{
+ *   headerRef?: HTMLElement;
+ *   sideBarRef?: HTMLElement;
  *   sideBarOpen?: boolean;
- *   setSideBarOpen?: UseState<boolean>[1];
  *   headerState?: HeaderState;
+ *   setHeaderRef?: UseState<HTMLElement>[1];
+ *   setSideBarRef?: UseState<HTMLElement>[1];
+ *   setSideBarOpen?: UseState<boolean>[1];
  *   setHeaderState?: UseState<HeaderState>[1];
  * }>}
  */
@@ -20,15 +24,24 @@ export const RootContext = createContext({});
 const RootLayout = ({ children }) => {
   const router = useRouter();
 
+  const [headerRef, setHeaderRef] = useState(null);
+  const [sideBarRef, setSideBarRef] = useState(null);
   const [sideBarOpen, setSideBarOpen] = useState(false);
   const [headerState, setHeaderState] = useState({ title: "Designer-App" });
 
   return (
     <RootContext.Provider
       value={{
+        // state
+        headerRef,
+        sideBarRef,
         sideBarOpen,
-        setSideBarOpen,
         headerState,
+
+        // set state
+        setHeaderRef,
+        setSideBarRef,
+        setSideBarOpen,
         setHeaderState,
       }}
     >
