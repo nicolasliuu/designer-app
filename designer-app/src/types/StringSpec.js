606d01a52fc686effb1a0d3b2597c84632b1a8c2
Fabio Fernandez <ffernan9@jhu.edu>
2024-11-13 15:46:03 -0500

    refactor string spec type > extend abstract spec type > make behavior consistent w/ other specs

--- designer-app/src/types/StringSpec.js
+++ designer-app/src/types/StringSpec.js
@@ -1,47 +1,63 @@
-export default class StringSpec {
-    /** @type {string} */
-    value = null;
-  
-    /** @type {number} */
-    minLength = 0;
-  
-    /** @type {number} */
-    maxLength = Number.MAX_SAFE_INTEGER;
-  
-    /**
-     * @param {Object} options
-     * @param {number} [options.minLength=0]
-     * @param {number} [options.maxLength=Number.MAX_SAFE_INTEGER]
-     * @param {string} [options.value=undefined]
-     */
-    constructor(options = {}) {
-      const { minLength = 0, maxLength = Number.MAX_SAFE_INTEGER, value } = options;
-      
-      this.minLength = minLength;
-      this.maxLength = maxLength;
-  
-      if (typeof value === 'string' && 
-          value.length >= minLength && 
-          value.length <= maxLength) {
-        this.value = value;
-      }
-    }
-  
-    /**
-     * @param {Object} [options]
-     * @param {number} [options.minLength]
-     * @param {number} [options.maxLength]
-     */
-    static defineSchema(options = {}) {
-      return new this(options).getSchema();
-    }
-  
-    getSchema() {
-      return {
-        class: StringSpec.name,
-        value: "string",
-        minLength: this.minLength,
-        maxLength: this.maxLength
-      };
-    }
+import AbstractSpecType from "@/types/AbstractSpecType";
+
+/**
+ * @extends {AbstractSpecType<VType>}
+ * @typedef {string} VType
+ */
+export default class StringSpec extends AbstractSpecType {
+  /** @type {number} */
+  minLength;
+
+  /** @type {number} */
+  maxLength;
+
+  /**
+   * @param {StringSpecOptions} options
+   * @param {string} value
+   */
+  constructor(options = {}, value = undefined) {
+    const { minLength = 0, maxLength = Number.MAX_SAFE_INTEGER } = options;
+
+    super("string", "");
+
+    this.minLength = minLength;
+    this.maxLength = maxLength;
+    this.setValue(value);
   }
+
+  /**
+   * @returns {typeof this.prototype}
+   * @override
+   */
+  static from(obj) {
+    return super.from(obj);
+  }
+
+  /**
+   * @param {StringSpecOptions} options
+   * @override
+   */
+  static defineSchema(options = {}) {
+    return new this(options).getSchema();
+  }
+
+  /**
+   * @param {VType} value
+   * @override
+   */
+  validate(value = this.default) {
+    const overMinLength = value?.length >= this.minLength;
+    const underMaxLength = value?.length <= this.maxLength;
+  
+    return (overMinLength && underMaxLength) ? `${value}` : this.default;
+  }
+
+  /** @override */
+  getSchema() {
+    return {
+      ...super.getSchema(),
+      minLength: this.minLength,
+      maxLength: this.maxLength,
+    };
+  }
+}
