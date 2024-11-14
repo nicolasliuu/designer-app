67ec79e2f3e0462cfaebb87baffa7ae36b1851ec
Fabio Fernandez <ffernan9@jhu.edu>
2024-11-13 22:45:21 -0500

    add loading state, remove edit inputs

--- designer-app/pages/index.js
+++ designer-app/pages/index.js
@@ -42,7 +42,6 @@ export default function Home() {
     axios
       .post(apiUrl, { prompt })
       .then(({ data }) => {
-        setGarmentId(data.id);
         setImgSrc(data.url);
       })
       .catch((err) => console.log(err))
