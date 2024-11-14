0a6cc6a005fd66a8096d3c1b82f58aaa7ad043c8
Nicolas Liu <nicolasliu18@gmail.com>
2024-11-13 23:44:34 -0500

    db for production

--- src/util/db.js
+++ src/util/db.js
@@ -1,19 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
-const PrismaClientSingleton = () => {
-  return new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "warn", "error"]
-        : ["error"],
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  // In production, create a new PrismaClient only if it doesn't already exist
+  prisma = globalThis.prismaGlobal ?? new PrismaClient({
+    log: ["error"], // Only log errors in production
   });
-};
-
-/** @type {ReturnType<typeof PrismaClientSingleton>} */
-const prisma = globalThis.prismaGlobal ?? PrismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prismaGlobal = prisma;
+  globalThis.prismaGlobal = prisma; // Save instance to avoid recreating
+} else {
+  // In development, recreate PrismaClient for each request to avoid issues with Hot Reloading
+  if (!globalThis.prismaGlobal) {
+    globalThis.prismaGlobal = new PrismaClient({
+      log: ["query", "warn", "error"], // Log more details for development
+    });
+  }
+  prisma = globalThis.prismaGlobal;
 }
 
 export default prisma;
