b8a729caacf0a24efd17886ebbf31319c864e0c7
Fabio Fernandez <ffernan9@jhu.edu>
2024-12-02 00:25:08 -0500

    [wip] assemble shirt puppet > 3 types of neck, 3 types of sleeve > sizing based on root unit within puppet

--- designer-app/pages/_app.js
+++ designer-app/pages/_app.js
@@ -1,9 +1,11 @@
 import RootLayout from "@/components/RootLayout";
+import { SessionProvider } from "next-auth/react";
+import { Rubik } from "next/font/google";
+
 import "@/styles/colors.css";
 import "@/styles/globals.css";
 import "@/styles/layout.css";
-import { SessionProvider } from "next-auth/react";
-import { Rubik } from "next/font/google";
+import "@/styles/shirt-puppet.css";
 import "overlayscrollbars/overlayscrollbars.css";
 import "tippy.js/dist/tippy.css";
 
--- designer-app/pages/garment/[encodedId]/edit/index.js
+++ designer-app/pages/garment/[encodedId]/edit/index.js
@@ -1,18 +1,32 @@
+"use client";
+
+import ShirtSVG from "@/assets/shirt/shirt.svg";
 import { RootContext } from "@/context/RootContext";
 import GarmentEncoder from "@/types/GarmentEncoder";
 import Shirt from "@/types/garments/Shirt";
 import { useBodyID } from "@/util/hooks";
+import clsx from "clsx";
 import { useRouter } from "next/router";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function Editor() {
   const { encodedId } = useRouter().query;
 
   const { setHeaderState, activeTask, setActiveTask } = useContext(RootContext);
 
+  const [neck, setNeck] = useState(null);
+  const [sleeve, setSleeve] = useState(null);
+
+  /** @type {typeof ShirtSVG} */
+  const NeckSVG = neck?.default;
+  /** @type {typeof ShirtSVG} */
+  const SleeveSVG = sleeve?.default;
+
   useBodyID("edit");
 
-  useEffect(() => setHeaderState({ title: "Designer-App" }), []);
+  useEffect(() => {
+    setHeaderState({ title: "Designer-App" });
+  }, []);
 
   useEffect(() => {
     if (typeof encodedId !== "string") return;
@@ -20,6 +34,7 @@ export default function Editor() {
     if (!activeTask?.garment) {
       // TODO: re-fetch garment on refresh
 
+      // testing with example garment
       /** @ts-ignore @type {Garment} */
       const garment = new Shirt().serialize();
       garment.id = GarmentEncoder.decode(encodedId);
@@ -28,5 +43,38 @@ export default function Editor() {
     }
   }, [encodedId]);
 
-  return <div className="edit-layout"></div>;
+  useEffect(() => {
+    console.log(sleeve);
+  }, [sleeve]);
+
+  useEffect(() => {
+    const garment = activeTask?.garment;
+    if (!garment) return;
+
+    getSVG("short-sleeve").then(setSleeve);
+    getSVG("v-neck").then(setNeck);
+  }, [activeTask?.garment]);
+
+  /** @param {string} asset */
+  function getSVG(asset) {
+    return import(`@/assets/shirt/${asset}.svg`);
+  }
+
+  return (
+    <div className="edit-layout">
+      <div className="garment-puppet">
+        {/* // TODO: move shirt puppet to its own component */}
+        <div className={clsx("shirt-puppet", !neck && "opacity-0")}>
+          {sleeve && (
+            <SleeveSVG className={clsx("sleeve", "left", SleeveSVG?.name)} />
+          )}
+          {sleeve && (
+            <SleeveSVG className={clsx("sleeve", "right", SleeveSVG?.name)} />
+          )}
+          <ShirtSVG className="shirt" />
+          {neck && <NeckSVG className="neck" />}
+        </div>
+      </div>
+    </div>
+  );
 }
